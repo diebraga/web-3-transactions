efeabbcc4d773a8fa6c3c06cda7cd90d21586916
Diego Braga <diebraga0055@gmail.com>
2023-12-24 22:29:23 +0000

    vite init

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-500">
+      <div>
+        <div className="text-red-500">Hello</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
