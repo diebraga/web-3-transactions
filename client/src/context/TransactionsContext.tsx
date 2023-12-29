091ab30a95cadf7f52c17c287d9f136768456b13
Diego Braga <diebraga0055@gmail.com>
2023-12-29 17:17:56 +0000

    trans test

--- client/src/context/TransactionsContext.tsx
+++ client/src/context/TransactionsContext.tsx
@@ -1,15 +1,12 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { ethers, Eip1193Provider } from "ethers";
-import {
-  contractAbi,
-  contractAddress,
-  formDataState,
-} from "../utils/constants";
+import { formDataState } from "../utils/constants";
 import { handleNoEthObjError } from "../utils/handleNoEthObjError";
 import {
   MetamaskMethodType,
   requestAccountsMetamask,
 } from "../utils/requestAccountsMetamask";
+import { createEthereumContract } from "../utils/createEthereumContract";
 
 declare global {
   interface Window {
@@ -28,6 +25,7 @@ interface TransactionsContextProps {
   setformData: React.Dispatch<React.SetStateAction<typeof formDataState>>;
   currAccount: string;
   formData: typeof formDataState;
+  isLoading: boolean;
 }
 
 export const TransactionsContext = createContext<TransactionsContextProps>(
@@ -37,30 +35,13 @@ export const TransactionsContext = createContext<TransactionsContextProps>(
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [currAccount, setCurrAccount] = useState("");
   const [formData, setformData] = useState(formDataState);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement>,
     name: string
   ) => {
     setformData((prevState) => ({ ...prevState, [name]: e.target.value }));
-  };
-
-  const createEthereumContract = async () => {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-    const transactionsContract = new ethers.Contract(
-      contractAddress,
-      contractAbi,
-      signer
-    );
-
-    console.log({
-      provider,
-      signer,
-      transactionsContract,
-    });
-
-    return transactionsContract;
   };
 
   const checkWalletConnection = async () => {
@@ -92,7 +73,49 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   };
 
   const sendTransaction = async () => {
-    await createEthereumContract();
+    console.log("trans called");
+
+    if (!window.ethereum) {
+      alert("Please download MetaMask!");
+      return;
+    }
+    const { addressTo, amount, keyword, message } = formData;
+    const value = ethers.parseEther(amount);
+
+    const transactionsContract = await createEthereumContract();
+
+    try {
+      await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: currAccount,
+            to: addressTo,
+            gas: "0x5208", // 21000 gwei
+            value,
+          },
+        ],
+      });
+
+      const transactionHash = await transactionsContract.addToBlockChain(
+        addressTo,
+        value,
+        message,
+        Date.now(),
+        keyword
+      );
+      setIsLoading(true);
+      console.log(`Loading transaction ${transactionHash.hash}`);
+      await transactionHash.wait();
+      setIsLoading(false);
+      console.log(`Loading success ${transactionHash.hash}`);
+
+      console.log(
+        `Transaction conunt: ${await transactionsContract.getTransactionsCount()}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -108,6 +131,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         formData,
         setformData,
         sendTransaction,
+        isLoading,
       }}
     >
       {children}
